59f610b41ef6886a8c85b8e2d9f43cc9f998d2e9
Xiaochuan Xu <xiaochuanxu@cmu.edu>
2024-11-07 21:38:32 -0500

    add more test

--- src/test/java/org/icepear/echarts/SnapshotWithEmptyChartTest.java
+++ src/test/java/org/icepear/echarts/SnapshotWithEmptyChartTest.java
@@ -7,8 +7,14 @@ public class SnapshotWithEmptyChartTest {
     private Option option = null;
 
     @Test(expected = UnsupportedConfigurationException.class)
-    public void testTakeSnapshot() throws Exception{
+    public void testTakeSnapshotThrowUnspportedChart() throws Exception{
         SnapshotSettingsBuilder builder = new SnapshotSettingsBuilder(option, "png");
+        Snapshot.takeSnapshot(builder);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidFileTypeShouldThrowException() throws Exception {
+        SnapshotSettingsBuilder builder = new SnapshotSettingsBuilder(option, "xxx");
         Snapshot.takeSnapshot(builder);
     }
 }
